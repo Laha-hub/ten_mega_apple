ba707901de243d9ee46cfc9171e089a9490e67d7
Laha-hub <81339797 +Laha-hub@users.noreply.github.com>
2021-12-31 23:34:10 +0900

    #5make cube work

--- script.js
+++ script.js
@@ -1,9 +1,59 @@
 'use strict';
 
 {
+    // cube
+    let x = 0,
+        y = 20,
+        z = 0,
+        bool = true,
+        interval;
+    const cube = document.querySelector('.cube');
+
+    document.querySelector('.top-x-control').addEventListener('click', () => {
+        cube.style.transform = `rotateX(${x += 20}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+    });
+    document.querySelector('.bottom-x-control').addEventListener('click', () => {
+        cube.style.transform = `rotateX(${x -= 20}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+    });
+    document.querySelector('.left-y-control').addEventListener('click', () => {
+        cube.style.transform = `rotateX(${x}deg) rotateY(${y -= 20}deg) rotateZ(${z}deg)`;
+    });
+    document.querySelector('.right-y-control').addEventListener('click', () => {
+        cube.style.transform = `rotateX(${x}deg) rotateY(${y += 20}deg) rotateZ(${z}deg)`;
+    });
+    document.querySelector('.top-z-control').addEventListener('click', () => {
+        cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z -= 20}deg)`;
+    });
+    document.querySelector('.bottom-z-control').addEventListener('click', () => {
+        cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z += 20}deg)`;
+    });
+
+    const playPause = () => {
+        if(bool) {
+            interval = setInterval(() => {
+                cube.style.transform = `rotateX(${x}deg) rotateY(${y++}deg) rotateZ(${z}deg)`;
+            }, 100);
+        } else {
+            clearInterval(interval);
+        }
+    };
+    playPause();
+
+    document.querySelector('.controls').addEventListener('mouseover', () => {
+        bool = false;
+        playPause();
+    })
+
+    document.querySelector('.controls').addEventListener('mouseout', () => {
+        bool = true;
+        playPause();
+    })
+    // end of cube
+
+
     // slideshow
     const slideshowDivs = () => {
-        for (let i = 1; i <= 5; i++) {
+        for(let i = 1; i <= 5; i++) {
             const div = document.createElement('div');
             div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`;
 
@@ -11,7 +61,7 @@
 
             document.querySelector('.slideshow').appendChild(div);
         }
-    }
+    };
     slideshowDivs();
 
     const divs = document.querySelectorAll('.slideshow div');
